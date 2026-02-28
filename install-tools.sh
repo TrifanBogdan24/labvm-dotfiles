9808dc675b225917e42fd7f6d1e621344383340e
TrifanBogdan24 <trifanbogdan24@gmail.com>
2026-02-28 16:07:30 +0200

    Passing '-y' to add-apt & no sudo

--- install-tools.sh
+++ install-tools.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # for noevim 0.10.0+
-sudo add-apt-repository ppa:neovim-ppa/unstable
+sudo add-apt-repository ppa:neovim-ppa/unstable -y
 
 apt update && apt upgrade -y
 apt install -y git wget curl git \
