2b962d78a20a60c3c4c04df40b35ff70d71b3bcd
TrifanBogdan24 <trifanbogdan24@gmail.com>
2026-03-04 14:24:26 +0200

    Removed 'apt upgrade' for a faster setup

--- install-tools.sh
+++ install-tools.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # for noevim 0.10.0+
-sudo add-apt-repository ppa:neovim-ppa/unstable -y
+add-apt-repository ppa:neovim-ppa/unstable -y
 
-apt update && apt upgrade -y
+apt update
 apt install -y git wget curl git \
     fzf neovim tmux zsh \
     cowsay lolcat
